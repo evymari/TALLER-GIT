cacfc1b5b046d8d761eda9e9eb343c590b90db7f
evymaritza <evymaritza3@gmail.com>
2024-05-13 19:31:14 +0200

    prueba

---
+++ reto-emilio.js
@@ -0,0 +1,9 @@
+let colorines = document.querySelector(".color")
+let saludo = ["adios","Biemvenido"];
+function Cambiar(){
+ colorines.style.color = "pink";
+}
+
+let cafe = document.createElement("p");
+     cafe.textContent=patata;
+     animales.appendChild(cafe);
